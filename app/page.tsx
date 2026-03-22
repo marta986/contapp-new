789527c3d4a376a9cb68dbf0dc22306b4b3e0df2
marta986 <144846584+marta986@users.noreply.github.com>
2026-03-22 04:36:21 -0400

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>ContApp funcionando 🚀</h1>
-      <p>Sistema contable en desarrollo</p>
-      <a href="/clientes">Ir a Clientes</a>
-    </main>
-  );
-}
